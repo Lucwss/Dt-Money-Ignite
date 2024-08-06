d8efe17900fa1807b85834d762e02df7c4ccf438
Lucas Emanoel Rodrigues da Silva <lucws2002@gmail.com>
2024-08-06 15:10:31 -0400

    feat: 🎸 add transaction type in transaction modal

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import * as Dialog from '@radix-ui/react-dialog';
-import { X } from "phosphor-react";
-import { CloseButton, Content, Overlay } from './styles';
+import { ArrowCircleDown, ArrowCircleUp, X } from "phosphor-react";
+import { CloseButton, Content, Overlay, TransactionType, TransactionTypeButton } from './styles';
 
 export function NewTransactionModal() {
   return (
@@ -18,8 +18,19 @@ export function NewTransactionModal() {
           <input type="number" placeholder="Price" required />
           <input type="text" placeholder="Categoty" required />
 
+          <TransactionType>
+            <TransactionTypeButton variant='income'>
+              <ArrowCircleUp size={24} />
+              Entrada
+            </TransactionTypeButton>
+            <TransactionTypeButton variant='outcome'>
+              <ArrowCircleDown size={24} />
+              Saída
+            </TransactionTypeButton>
+          </TransactionType>
+
           <button type="submit">
-            Cadastrar
+            Register
           </button>
         </form>
       </Content>
